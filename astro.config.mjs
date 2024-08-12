53cacc1b54c18572d9ae23e953e6a6f03b599fad
Matthew Milunic <mmilunicmobile@gmail.com>
2024-08-11 22:55:00 -0400

    Initial commit.

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+
+import solidJs from "@astrojs/solid-js";
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://mathuml.mooo.com',
+    integrations: [mdx({
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex]
+    }), solidJs()]
+});
